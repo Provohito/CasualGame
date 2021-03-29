dc48f5c2dd45986a7a394d5217685b68f8cf3211
Alex Semchenkov <zigola12@list.ru>
2021-03-29 17:33:26 +0300

    Create animation menu Icons

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    
+    Animator menuAnimator;  // переменная для получения аниматора из назначенного объекта
+
+    void Start()
+    {
+       
+    }
+    public void OpenSettings(GameObject objWithAnimator) // Получаем из объекта в инспекторе аниматор и запускаем соответствующую анимацию
+    {
+        menuAnimator = objWithAnimator.GetComponent<Animator>();
+        if (menuAnimator.GetBool("isHide") == true)
+        {
+            menuAnimator.Play("open");
+            menuAnimator.SetBool("isHide", false);  
+        }
+        else
+        {
+            menuAnimator.Play("close");
+            menuAnimator.SetBool("isHide", true); 
+        }
+    }
+}
