7f606f50c04cc903948d5c4b354f49519c80f7eb
Alex Semchenkov <zigola12@list.ru>
2021-04-13 14:09:51 +0300

    fix bag with rendering plaeyrSprite
    Canvas take camera render, order layer layer 0, player Render 1 order layer

--- Assets/Scripts/CoreSceneScripts/PlayerController.cs
+++ Assets/Scripts/CoreSceneScripts/PlayerController.cs
@@ -12,23 +12,22 @@ public class PlayerController : MonoBehaviour
     bool playerEnable;
     void Update()
     {
-        if (Input.GetKey(KeyCode.Mouse0))
-        {
+        
+    }
 
-            if (playerEnable)
-            {
-                return;
-            }
-            else
-            {
-                MousePos = Input.mousePosition;
-                Debug.Log(MousePos);
-                var player = Instantiate(playerPrefab);
-                var world = Camera.main.ScreenToWorldPoint(MousePos);
-                player.transform.position = new Vector3(world.x, world.y, 0);
-                playerEnable = true;
-            }
-            
+    public void MovingPlayer()
+    {
+        if (playerEnable)
+        {
+            return;
+        }
+        else
+        {
+            MousePos = Input.mousePosition;
+            var player = Instantiate(playerPrefab);
+            var world = Camera.main.ScreenToWorldPoint(MousePos);
+            player.transform.position = new Vector3(world.x, world.y, 0);
+            playerEnable = true;
         }
     }
 }
