6c9616f771b0c7c32df58a20fa059f533ba72a4b
Alex Semchenkov <zigola12@list.ru>
2021-04-13 15:24:08 +0300

    Made shoot system

--- Assets/Scripts/CoreSceneScripts/PlayerController.cs
+++ Assets/Scripts/CoreSceneScripts/PlayerController.cs
@@ -10,24 +10,42 @@ public class PlayerController : MonoBehaviour
 
     private Vector2 MousePos;
     bool playerEnable;
+    GameObject player;
+
+    Rigidbody2D playerRB;
+
+
+    void Awake()
+    {
+        player = Instantiate(playerPrefab, this.transform);
+        playerRB = player.GetComponent<Rigidbody2D>();
+    }
     void Update()
     {
+        if (Input.GetMouseButtonDown(0))
+        {
+            MousePos = Input.mousePosition;
+            var world = Camera.main.ScreenToWorldPoint(MousePos);
+            player.transform.position = new Vector3(world.x, world.y, 0);
+        }
         
     }
 
     public void MovingPlayer()
     {
-        if (playerEnable)
-        {
-            return;
-        }
-        else
-        {
-            MousePos = Input.mousePosition;
-            var player = Instantiate(playerPrefab);
-            var world = Camera.main.ScreenToWorldPoint(MousePos);
-            player.transform.position = new Vector3(world.x, world.y, 0);
-            playerEnable = true;
-        }
+        MousePos = Input.mousePosition;
+
+        var world = Camera.main.ScreenToWorldPoint(MousePos);
+            if (Input.GetMouseButtonUp(0))
+            {
+                Shoot(3);
+            }
+
+    }
+
+    void Shoot(int speed)
+    {
+        Debug.Log("up");
+        playerRB.velocity = new Vector2(0f, speed);
     }
 }
